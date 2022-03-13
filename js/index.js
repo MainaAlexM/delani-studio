5bebefea95a68e3293f387de07084e80106d5d38
Mathenge-Alex <arxmaina@gmail.com>
2022-03-13 15:02:07 +0300

    func: add javascript code for tggling the What We Do icons,

--- js/index.js
+++ js/index.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
-    $(".role1 img, .role1 p").click(function() {
+    // Toggle for task 1
+    $(".click1").click(function() {
         $(".role1").toggle();
-        $(".hidden-role1").toggle();
+        $(".hidden1").toggle();
     });
 
-    // $(".hidden-role1 h3, .hidden-role1 p").click(function(){
-    //     $("role1").toggle();
-    // });
+    $(".hidden1-h3").click(function(){
+        $(".hidden1").toggle();
+        $(".role1").toggle();
+    });
     
+    // Toggle for task 2
+    $(".click2").click(function() {
+        $(".role2").toggle();
+        $(".hidden2").toggle();
+    });
+
+    $(".hidden2-h3").click(function(){
+        $(".hidden2").toggle();
+        $(".role2").toggle();
+    });
+
+    // Toggle for task 3
+    $(".click3").click(function() {
+        $(".role3").toggle();
+        $(".hidden3").toggle();
+    });
+
+    $(".hidden3-h3").click(function(){
+        $(".hidden3").toggle();
+        $(".role3").toggle();
+    });
 });
