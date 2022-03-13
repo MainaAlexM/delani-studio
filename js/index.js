427b38f3a9e1c20730ca94b6a97c678eb7d4b0c6
Mathenge-Alex <arxmaina@gmail.com>
2022-03-13 14:02:24 +0300

    add hidden-role divs

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".role1 img, .role1 p").click(function() {
+        $(".role1").toggle();
+        $(".hidden-role1").toggle();
+    });
+
+    // $(".hidden-role1 h3, .hidden-role1 p").click(function(){
+    //     $("role1").toggle();
+    // });
+    
+});
