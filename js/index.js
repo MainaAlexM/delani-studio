8cc796ebb4efb76601d604a3643c182101377716
Mathenge-Alex <arxmaina@gmail.com>
2022-03-14 07:33:29 +0300

    add contact response, update navbar, and site responsiveness

--- js/index.js
+++ js/index.js
@@ -37,4 +37,40 @@ $(document).ready(function() {
 var date = new Date();
     year = date.getFullYear();
 
-    document.getElementById("year").innerHTML = year;      
+    document.getElementById("year").innerHTML = year;
+
+    // ............... Form Reset
+function resetForm(){
+    document.getElementById('userRequest').reset();
+    }
+    
+    // ............. Validation
+function validate(){
+    document.getElementById("userRequest")
+    var userName = document.getElementById("name").value
+    var mail = document.getElementById("mail").value
+    var userMessage = "hello " + userName + ", We have received your submission. We will send you feedback on your email address: " + mail + ". We treasure you!";
+    if (userName =="" || mail == ""){
+        alert("Please fill in all the fields before submitting");
+        return false;
+    }
+    else if(userName != "" && mail !="" && userMessage ==""){
+        alert(userMessage);
+    } else{
+        alert(userMessage);
+    }
+
+    resetForm();
+}   
+
+// Window Navbar Scrolling
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
